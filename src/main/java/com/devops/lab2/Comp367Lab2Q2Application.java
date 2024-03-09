b1e6d07704516fef8b249b2c6057eee23aab3829
Visal Mohan <vmohan3@my.centennialcollege.ca>
2024-03-08 21:26:27 -0500

    Initial commit

---
+++ src/main/java/com/devops/lab2/Comp367Lab2Q2Application.java
@@ -0,0 +1,13 @@
+package com.devops.lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp367Lab2Q2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp367Lab2Q2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/lab2/Comp367Lab2Q2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp367Lab2Q2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
